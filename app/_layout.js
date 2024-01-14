3e3af723237f144d8f05add7a28f4bf6027b1e00
Neil Adams <neil.zhao.adams@gmail.com>
2024-01-14 06:12:31 -0800

    layout changed

--- app/_layout.js
+++ app/_layout.js
@@ -6,7 +6,11 @@ import * as SplashScreen from "expo-splash-screen";
 SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-  const [fontsLoaded] = useFonts({});
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
 
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
