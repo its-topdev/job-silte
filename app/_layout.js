d09c0fbf90147bdb9c51afc554015018bb53533a
Neil Adams <neil.zhao.adams@gmail.com>
2024-01-14 06:05:59 -0800

    layout changed

---
+++ app/_layout.js
@@ -0,0 +1,22 @@
+import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+  const [fontsLoaded] = useFonts({});
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return <Stack onLayout={onLayoutRootView} />;
+};
+
+export default Layout;
